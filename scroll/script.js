2dd6ec86d72592cac048d4aad50ecaa34353495e
Pablo Lorite <earlriti@gmail.com>
2026-03-20 00:49:22 +0100

    Add scroll and rename database folder to bd

---
+++ scroll/script.js
@@ -0,0 +1,10 @@
+/*
+  BLOQUE ÚNICO DE JAVASCRIPT
+
+  En este paso tampoco hace falta JavaScript.
+  El vídeo se reproduce solo gracias a los atributos
+  que hemos puesto en la etiqueta <video>.
+
+  Dejamos el archivo preparado por si en el siguiente paso
+  añadimos animación, scroll o interacción.
+*/
